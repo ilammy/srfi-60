805bc0a234ddcde92f9771337dd48d1b78d745b4
ilammy <a.lozovsky@gmail.com>
2015-04-09 00:21:53 +0300

    Include tests into the snowball

--- srfi-60/package.scm
+++ srfi-60/package.scm
@@ -8,4 +8,60 @@
     (path "srfi/60.sld")
     (depends
       (scheme base)
-      (scheme case-lambda))))
+      (scheme case-lambda))
+    (test-program "test/srfi-60-tests.scm")
+    (test-program "test/generated/srfi-60-generated-4.scm")
+    (test-program "test/generated/srfi-60-generated-8.scm")
+    (test-program "test/generated/srfi-60-generated-16.scm")
+    (test-program "test/generated/srfi-60-generated-32.scm")
+    (test-program "test/generated/srfi-60-generated-256.scm")
+    (test-program "test/generated/srfi-60-generated-1024.scm"))
+  (program
+    (use-for test)
+    (path "test/srfi-60-tests.scm")
+    (depends
+      (chibi)
+      (chibi test)
+      (srfi 60)))
+  (program
+    (use-for test)
+    (path "test/generated/srfi-60-generated-4.scm")
+    (depends
+      (chibi)
+      (chibi test)
+      (srfi 60)))
+  (program
+    (use-for test)
+    (path "test/generated/srfi-60-generated-8.scm")
+    (depends
+      (chibi)
+      (chibi test)
+      (srfi 60)))
+  (program
+    (use-for test)
+    (path "test/generated/srfi-60-generated-16.scm")
+    (depends
+      (chibi)
+      (chibi test)
+      (srfi 60)))
+  (program
+    (use-for test)
+    (path "test/generated/srfi-60-generated-32.scm")
+    (depends
+      (chibi)
+      (chibi test)
+      (srfi 60)))
+  (program
+    (use-for test)
+    (path "test/generated/srfi-60-generated-256.scm")
+    (depends
+      (chibi)
+      (chibi test)
+      (srfi 60)))
+  (program
+    (use-for test)
+    (path "test/generated/srfi-60-generated-1024.scm")
+    (depends
+      (chibi)
+      (chibi test)
+      (srfi 60))))
