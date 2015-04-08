1947b4dc9a8157cd9c00c6b156feb86fc527088e
ilammy <a.lozovsky@gmail.com>
2015-04-08 23:09:58 +0300

    Snowball description and packaging utility

---
+++ srfi-60/package.scm
@@ -0,0 +1,11 @@
+(package
+  (name (srfi 60))
+  (description "SRFI 60 'Integers as Bits'")
+  (homepage "https://github.com/ilammy/srfi-60")
+  (authors "ilammy")
+  (library
+    (name (srfi 60))
+    (path "srfi/60.sld")
+    (depends
+      (scheme base)
+      (scheme case-lambda))))
